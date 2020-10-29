2d704685d17c4e2a0f0bb3468686ac81c27f9304
Patrick <pat.nolan@heronsystems.com>
2020-10-29 13:00:09 -0400

    JSBSim port fixes. Also adding multi-vehicle script.

--- Tools/autotest/sim_multi_vehicle.py
+++ Tools/autotest/sim_multi_vehicle.py
@@ -10,31 +10,39 @@ import json
 if __name__ == '__main__':
     # Parse command line arguments:
     parser = argparse.ArgumentParser()
-    parser.add_argument('fp', type=str, help='Relative file path to the launch script JSON configuration file. (e.g.: ./config_1_vehicle.json')
-    config = parser.parse_args()
-    with open(config.fp, 'r') as in_file:
-        config = json.load(in_file)
+    parser.add_argument('-n', type=int, default=1, help='Number of vehicles to simulate')
+    parser.add_argument('-type', type=str, default="ArduPlane", help='Type of vehicle. NOTE: If set to ArduCopter, the jsbVehicle flag is ignored')
+    parser.add_argument('-jsbVehicle', type=str, default="Talon", help='Type of JSBSim vehicle model to load')
+    parser.add_argument('-console', type=bool, default=False, help='Display ardupilot console')
+    args = parser.parse_args()
 
-    vehicles = config['vehicles']
+    system('tmux new -d -s vehicles')
+    for i in range(args.n):        
+        vehicleID = i+1
+        print("____________ Launch {} with Vehicle ID = {} ____________".format(args.type, vehicleID))
 
-    ## Options for {vehicle_type}: ArduCopter|AntennaTracker|APMrover2|ArduSub|ArduPlane
-    ## Options for {frame_type} depend on vehicle type. For example, for ArduCopter: octa-quad|tri|singlecopter|gazebo-iris|calibration|hexa|heli|+|heli-compound|dodeca-hexa|heli-dual|coaxcopter|X|quad|y6|IrisRos|octa
+        time.sleep(3)
+        windowName = '{}_{}'.format(args.type, vehicleID)
+        system('tmux new-window -n {}'.format(windowName))
+        vehicleType = args.type
+        jsbsimVehicle = args.jsbVehicle
+        outputIP = "127.0.0.1"
+        outputPort = 14550 + vehicleID
+        lat = -35.3632621 + vehicleID/10
+        lng = 149.1652374 + vehicleID/100
+        alt = 0.0
+        heading = 0.0
 
-    # Loop over vehicle ID/sensor array to extend the command to launch each vehicle according to their type
-    if len(vehicles.items()) > 0:
-		system('tmux new -d -s vehicles')
-		for key, value in vehicles.items():
-			print("____________ Launch quad with Vehicle ID = {} ____________".format(key))
-			location = value["position"]
-
-			if value["simulated"] == True:
-				time.sleep(1)
-				windowName = '{}_{}'.format(value["type"], key)
-				system('tmux new-window -n {}'.format(windowName))
-				if value["console"] == True:
-					system('tmux send-keys -t {} "cd ardupilot/ArduCopter; sim_vehicle.py --custom-location={},{},{},{} --out=udp:{}:{} --instance={} --vehicle={} --frame={} --console" ENTER'.format(windowName, location["lat"], location["lng"], location["alt_msl"], location["heading_deg"], value["outputIP"], value["outputPort"], key, value["type"], value["frame"]))
-				else:
-					system('tmux send-keys -t {} "cd ardupilot/ArduCopter; sim_vehicle.py --custom-location={},{},{},{} --out=udp:{}:{} --instance={} --vehicle={} --frame={}" ENTER'.format(windowName, location["lat"], location["lng"], location["alt_msl"], location["heading_deg"], value["outputIP"], value["outputPort"], key, value["type"], value["frame"]))
+        if args.console == True:
+            if args.type == "ArduCopter":
+                system('tmux send-keys -t {} "sim_vehicle.py --custom-location={},{},{},{} --out=udp:{}:{} --instance={} --sysid={} --vehicle={} --console" ENTER'.format(windowName, lat, lng, alt, heading, outputIP, outputPort, vehicleID, vehicleID, vehicleType))
+            else:
+                system('tmux send-keys -t {} "sim_vehicle.py --custom-location={},{},{},{} --out=udp:{}:{} --instance={} --sysid={} --vehicle={} --console -f jsbsim:{}" ENTER'.format(windowName, lat, lng, alt, heading, outputIP, outputPort, vehicleID, vehicleID, vehicleType, jsbsimVehicle))
+        else:
+            if args.type == "ArduCopter":
+                system('tmux send-keys -t {} "sim_vehicle.py --custom-location={},{},{},{} --out=udp:{}:{} --instance={} --sysid={} --vehicle={}" ENTER'.format(windowName, lat, lng, alt, heading, outputIP, outputPort, vehicleID, vehicleID, vehicleType))
+            else:
+                system('tmux send-keys -t {} "sim_vehicle.py --custom-location={},{},{},{} --out=udp:{}:{} --instance={} --sysid={} --vehicle={} -f jsbsim:{}" ENTER'.format(windowName, lat, lng, alt, heading, outputIP, outputPort, vehicleID, vehicleID, vehicleType, jsbsimVehicle))
 
 
     # Attach to tmux:
