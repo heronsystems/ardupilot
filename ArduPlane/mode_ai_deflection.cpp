b1a95ee6b22248e1154f4c07ff2bcd122e1716d1
Patrick <pat.nolan@heronsystems.com>
2021-01-27 13:26:53 -0500

    Commented out test code

--- ArduPlane/mode_ai_deflection.cpp
+++ ArduPlane/mode_ai_deflection.cpp
@@ -39,6 +39,18 @@ void ModeAI_Deflection::update()
     SRV_Channels::set_output_scaled(SRV_Channel::k_throttle, _servoOutput.stickThrottle);
 
     // plane.steering_control.steering = plane.steering_control.rudder = _servoOutput.stickRudder;
+
+    // // Test to fix sim:
+    // uint16_t PWMAileron, PWMElevator, PWMThrottle, PWMRudder;
+    // SRV_Channels::get_output_pwm(SRV_Channel::k_aileron, PWMAileron);
+    // SRV_Channels::get_output_pwm(SRV_Channel::k_elevator, PWMElevator);
+    // SRV_Channels::get_output_pwm(SRV_Channel::k_throttle, PWMThrottle);
+    // SRV_Channels::get_output_pwm(SRV_Channel::k_rudder, PWMRudder);
+    // // Set PWM outputs:
+    // SRV_Channels::set_output_pwm(SRV_Channel::k_aileron, PWMAileron);
+    // SRV_Channels::set_output_pwm(SRV_Channel::k_elevator, PWMElevator);
+    // SRV_Channels::set_output_pwm(SRV_Channel::k_throttle, PWMThrottle);
+    // SRV_Channels::set_output_pwm(SRV_Channel::k_rudder, PWMRudder);
 }
 
 bool ModeAI_Deflection::is_AI_control() const
@@ -72,12 +84,12 @@ void ModeAI_Deflection::handleMessage(const mavlink_execute_surface_deflection_o
                          packet.deflection_rudder, PWMRudder,
                          packet.deflection_throttle, PWMThrottle);
 
-    // int16_t PWMAileron1 = 0, PWMElevator1 = 0, PWMThrottle1 = 0, PWMRudder1 = 0;
+    int16_t PWMAileron1 = 0, PWMElevator1 = 0, PWMThrottle1 = 0, PWMRudder1 = 0;
 
-    // mapToDeflection(plane.channel_pitch, packet.deflection_elevator, 1, 2, false, PWMElevator1);
-    // mapToDeflection(plane.channel_rudder, packet.deflection_rudder, 1, 2, false, PWMRudder1);
-    // mapToDeflection(plane.channel_roll, packet.deflection_aileron, 1, 2, false, PWMAileron1);
-    // mapToDeflection(plane.channel_throttle, packet.deflection_throttle, 0, 1, false, PWMThrottle1);
+    mapToDeflection(plane.channel_pitch, packet.deflection_elevator, 1, 2, false, PWMElevator1);
+    mapToDeflection(plane.channel_rudder, packet.deflection_rudder, 1, 2, false, PWMRudder1);
+    mapToDeflection(plane.channel_roll, packet.deflection_aileron, 1, 2, false, PWMAileron1);
+    mapToDeflection(plane.channel_throttle, packet.deflection_throttle, 0, 1, false, PWMThrottle1);
 
     // printf("Servo output (1): \n%d / %d / %d / %d\n", PWMAileron1, PWMElevator1, PWMThrottle1, PWMRudder1);
     // printf("Servo output: \n%d / %d / %d / %d\n", PWMAileron, PWMElevator, PWMThrottle, PWMRudder);
